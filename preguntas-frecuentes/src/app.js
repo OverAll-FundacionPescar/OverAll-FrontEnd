b876f9dbd8d5b05982121298cbe21002e397c51b
sofianu06 <sofianunez0603@gmail.com>
2024-09-20 16:40:02 -0300

    actualizacion preguntas frescuentes

--- preguntas-frecuentes/src/app.js
+++ preguntas-frecuentes/src/app.js
@@ -1,19 +1,33 @@
 let preguntas = document.querySelectorAll('.preguntas');
-let btnDropdown = document.querySelectorAll('.preguntas .more');
+let btnDropdown = document.querySelectorAll('.preguntas .more')
 let respuesta = document.querySelectorAll('.respuesta');
 let parrafo = document.querySelectorAll('.respuesta p');
 
-for ( let i = 0; i < btnDropdown.length; i ++) {
+for ( let i = 0; i < btnDropdown.length; i ++ ) {
 
     let altoParrafo = parrafo[i].clientHeight;
     let switchc = 0;
 
-    btnDropdown[i].addEventListener('click',  () =>{ 
+    btnDropdown[i].addEventListener('click', () => {
 
-        if ( switchc == o){
+        if ( switchc == 0 ) {
 
-            respuesta[i].style.height = '$(altoParrafo)px'
+            respuesta[i].style.height = `${altoParrafo}px`;
+            preguntas[i].style.marginBottom = '10px';
+            
+            switchc ++;
+
         }
 
+        else if ( switchc == 1 ) {
+
+            respuesta[i].style.height = `0`;
+            preguntas[i].style.marginBottom = '0';
+            
+            switchc --;
+            
+            }
+
     })
+
 }
